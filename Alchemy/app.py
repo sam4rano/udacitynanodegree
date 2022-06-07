9cc60db098990caec3efa5c1b375bffc86e89e52
Oyerinde Samuel <oyerindesamuelabiodun@gmail.com>
2022-06-07 20:14:28 +0100

    s

--- Alchemy/app.py
+++ Alchemy/app.py
@@ -1,30 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for
+import sys
+from flask import Flask, abort, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
 db = SQLAlchemy(app)
-
-
 migrate = Migrate(app, db)
 
 class Todo(db.Model):
-    __tablename__ ='todos' 
+    __tablename__ ='todo' 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     
     def __repr__(self):
-        return f'<Person ID: {self.id}, name:{self.name}>'
+        return f'<Todo ID: {self.id}, name:{self.description}>'
 
 # db.create_all() since we are using flask-migrate no need to use db_create_all()
 
+# @app.route('/')
+# def index():
+#     todo = Todo.query.first()
+#     return 'Hello Big ' + todo.name 
+
+@app.route('/todo/create', methods=['POST'])
+def create_todo():   
+   body={}
+   error = False
+   try: 
+       description =  request.get_json()['description']
+       todo = Todo(description=description)
+       body['description'] = todo.description
+       db.session.add(todo)
+       db.session.commit()
+   except:        
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+   finally:
+        db.session.close()           
+        if  error == True:
+            abort(400)
+        else:            
+            return jsonify(body)
+
 @app.route('/')
 def index():
-    person = Person.query.first()
-    return 'Hello Big ' + person.name 
+    return render_template('index.html', data=Todo.query.all())
+
 
 
 if __name__ == '__main__':
