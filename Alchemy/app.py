e96562108c690c9fab46a63bf17c5aa3a0e01a0d
Oyerinde Samuel <oyerindesamuelabiodun@gmail.com>
2022-06-07 18:50:48 +0100

    migrations changes

--- Alchemy/app.py
+++ Alchemy/app.py
@@ -1,20 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
-class Person(db.Model):
-    __tablename__ ='persons'
+
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+    __tablename__ ='todos' 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     
     def __repr__(self):
         return f'<Person ID: {self.id}, name:{self.name}>'
 
-db.create_all()
+# db.create_all() since we are using flask-migrate no need to use db_create_all()
 
 @app.route('/')
 def index():
